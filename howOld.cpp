be762f3292a1a1cf0817e73e4d30bea2e5bed253
moritaoy <moritaoy@outlook.jp>
2019-07-10 21:34:59 +0900

    add howOld.cpp

---
+++ howOld.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout<<"How old are you?"<<endl;
+  int x;
+  cin>>x;
+  if(x>=20)cout<<"Oh! You are older than me!"<<endl;
+  else if(x==19)cout<<"Oh! You are the same old as me!"<<endl;
+  else cout<<"Oh! You are younger than me! ...really?"<<endl;
+  return 0;
+}
